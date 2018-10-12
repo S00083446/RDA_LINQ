8108f912e53afb895b25d325ab5052fe362c78bc
s00083446 <s00083446@mail.itsligo.ie>
2018-10-12 18:23:34 +0100

    first commit

--- Exercise 2 - LINQ/ProductModel.cs
+++ Exercise 2 - LINQ/ProductModel.cs
@@ -16,19 +16,19 @@ namespace Exercise_2___LINQ
             UnitPrice = 0.10f,
             CategoryID = 1},
 
-            new Product {ProductID = 1,
+            new Product {ProductID = 2,
             Description = "9\" Bolts",
             QuantityInStock = 120,
             UnitPrice = 0.20f,
             CategoryID = 1},
 
-            new Product {ProductID = 1,
+            new Product {ProductID = 3,
             Description = "Chimney Hoover",
             QuantityInStock = 10,
             UnitPrice = 100.50f,
             CategoryID = 2},
 
-            new Product {ProductID = 1,
+            new Product {ProductID = 4,
             Description = "Washing Machine",
             QuantityInStock = 7,
             UnitPrice = 399.99f,
@@ -38,13 +38,15 @@ namespace Exercise_2___LINQ
 
        public List<Supplier> Suppliers = new List<Supplier>
         {
-            new Supplier {SupplierID = 1,
+            new Supplier {
+                SupplierID = 1,
                 SupplierName = "Parts 1",
                 SupplierAddressLine1 = "Addr 11",
                 SupplierAddressLine2 = "Addr 21",
             },
-                 new Supplier {SupplierID = 2,
-                 SupplierName = "Parts 2",
+                 new Supplier {
+                SupplierID = 2,
+                SupplierName = "Parts 2",
                 SupplierAddressLine1 = "Addr 11",
                 SupplierAddressLine2 = "Addr 21",
             }
@@ -52,29 +54,39 @@ namespace Exercise_2___LINQ
     
       public  List<Category> Categories = new List<Category>
         {
-            new Category {id = 1,
+            new Category {
+            id = 1,
             Description = "Hardware"}
             ,
-            new Category{id = 2,
+            new Category{
+            id = 2,
             Description = "Electrical Appliances"}
         };
 
-        List<SupplierProduct> SupplierProducts = new List<SupplierProduct>
+      public  List<SupplierProduct> SupplierProducts = new List<SupplierProduct>
         {
-            new SupplierProduct{ SupplierID = 1,
-            ProductID = 1, DateFirstSupplied = DateTime.Parse("12/12/2012"),
+            new SupplierProduct{
+            SupplierID = 1,
+            ProductID = 1,
+            DateFirstSupplied = DateTime.Parse("12/12/2012"),
             },
 
-             new SupplierProduct{ SupplierID = 1,
-            ProductID = 2, DateFirstSupplied = DateTime.Parse("06/01/2013"),
+            new SupplierProduct{
+            SupplierID = 1,
+            ProductID = 2,
+            DateFirstSupplied = DateTime.Parse("06/01/2013"),
             },
 
-              new SupplierProduct{ SupplierID = 2,
-            ProductID = 3, DateFirstSupplied = DateTime.Parse("09/09/2017"),
+            new SupplierProduct{
+            SupplierID = 2,
+            ProductID = 3,
+            DateFirstSupplied = DateTime.Parse("09/09/2017"),
             },
 
-               new SupplierProduct{ SupplierID = 2,
-            ProductID = 4, DateFirstSupplied = DateTime.Parse("10/09/2017"),
+            new SupplierProduct{
+            SupplierID = 2,
+            ProductID = 4,
+            DateFirstSupplied = DateTime.Parse("10/09/2017"),
             },
         };
 
--- Exercise 2 - LINQ/Program.cs
+++ Exercise 2 - LINQ/Program.cs
@@ -1,73 +1,81 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Exercise_2___LINQ
 {
-    class Program
+class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        // QUERIES
+        //1 - List all categories
+        Console.WriteLine("Question 1:");
+
+        ProductModel pModel = new ProductModel();
+
+        foreach (var item in pModel.Categories)
         {
-            // QUERIES
-            //1 - List all categories
-            Console.WriteLine("Question 1:");
+            Console.WriteLine(item);
+        }
 
-            ProductModel pModel = new ProductModel();
-            
-            foreach (var item in pModel.Categories)
-            {
-                Console.WriteLine(item);
-            }
+        //2 -  List all the Products
+        Console.WriteLine("\nQuestion 2");
+        foreach (var item in pModel.Products)
+        {
+            Console.WriteLine(item);
+        }
 
-            //2 -  List all the Products
-            Console.WriteLine("\nQuestion 2");
-            foreach (var item in pModel.Products)
-            {
-                Console.WriteLine(item);
-            }
+        //3 - List Products with Quantity <= 100
+        Console.WriteLine("\nQuestion 3");
 
-            //3 - List Products with Quantity <= 200
-            Console.WriteLine("\nQuestion 3");
+        var query = pModel.Products.Where(p => p.QuantityInStock <= 100);
+        foreach (var item in query)
+        {
+            Console.WriteLine(item);
+        }
 
-            var query = pModel.Products.Where(p => p.QuantityInStock <= 100);
-            foreach (var item in query)
-            {
-                Console.WriteLine(item);
-            }
+        //4 - List all Products together with their total value
+        Console.WriteLine("\nQuestion 4");
 
-            //4 - List all Products together with their total value
-            Console.WriteLine("\nQuestion 4");
+        float totalPrice;
+        foreach (var item in pModel.Products)
+        {
+            Console.WriteLine(item);
+            totalPrice = item.UnitPrice * item.QuantityInStock;
+            Console.Write("Total: {0:c}\n", totalPrice);
+        }
 
-            float totalPrice;
-            var total = pModel.Products.Sum(s => s.UnitPrice);
-            foreach (var item in pModel.Products)
-            {
-                Console.WriteLine(item);
-                totalPrice = item.UnitPrice * item.QuantityInStock;
-                Console.Write("Total: {0:f}\n", totalPrice);
-            }
+        //5 - List all the Products in the Hardware Category
+        Console.WriteLine("\nQuestion 5");
 
-
-            //5 - List all the Products in the Hardware Category
-            Console.WriteLine("\nQuestion 5");
-
-            var query5 = from c in pModel.Categories
+        var query5 = from c in pModel.Categories
                         join p in pModel.Products
                         on c.id equals p.CategoryID
                         where c.Description == "Hardware"
                         select p;
 
-            foreach (var item in query5)
-            {
-                Console.WriteLine(item);
-            }
-
-            // 6 - List all the suppliers and their Parts oredred by supplier name
-            Console.WriteLine("\nQuestion 6");
-
-
+        foreach (var item in query5)
+        {
+            Console.WriteLine(item);
         }
+
+        // 6 - List all the suppliers and their Parts oredred by supplier name
+        Console.WriteLine("\nQuestion 6");
+        var suppliers = (from s in pModel.Suppliers
+                            join sp in pModel.SupplierProducts
+                            on s.SupplierID equals sp.SupplierID
+                            join p in pModel.Products
+                            on sp.ProductID equals p.ProductID
+                            select new
+                            {   s.SupplierName,
+                                p.Description
+                            }).OrderBy(o => o.SupplierName);
+        foreach (var item in suppliers)
+        {
+            Console.WriteLine("Supllier name: " + item.SupplierName + "\t\tParts: " + item.Description);
+        }
+
+        Console.ReadKey();
     }
+}
 }
